8608f64c3e5a0d513206330d0e678e68f37bf0ec
Guillermo <md.guillermo1@gmail.com>
2025-04-01 21:06:16 +0200

    update

--- app/run.py
+++ app/run.py
@@ -1,8 +1,11 @@
-from models import Diario
-from werkzeug.utils import redirect
+
 from flask import Flask, render_template, request, url_for
 
-app = Flask(__name__)
+app=Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
