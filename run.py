fc7c5f89f6bde052b1f1e58b89007a1b2817e8d5
Guillermo <md.guillermo1@gmail.com>
2025-04-04 13:32:32 +0200

    funcionamiento principal terminado

--- run.py
+++ run.py
@@ -1,38 +1,36 @@
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, url_for,session
+from flask import Flask, render_template, request, session
 from app.models import Diario
 from app.model_mL.predictor import prediccion_diario
 from app import db
-from plotly import utils
-from json import dumps
 import pandas as pd
+import plotly.express as px
 
 app=Flask(__name__,template_folder='app/templates',static_folder='app/static')
 
+#Funcion para genearar grafico interactivo
+def generar_grafico():
+    datos = db.session.query(Diario.emocion,Diario.id,Diario.frase).all()
+    emocion = [e[0] for e in datos]
+    id = [i[1] for i in datos ]
+    frase = [f[2] for f in datos]
+    df_grafico = pd.DataFrame({
+        'Id':id,
+        'Frase':frase,
+        'Emocion':emocion
+    })
+    fig = px.scatter(df_grafico,x='Id', y='Emocion',title='Estadistica de emociones', hover_data={'Id': False, 'Emocion': False,'Frase':True} )
+    
+    return fig.to_html(full_html=False)
+
+#Creamos la home 
 @app.route("/")
 def home():
-    import matplotlib
-    import plotly.express as px
+    graph_html= generar_grafico()
     
-    emociones = db.session.query(Diario.emocion,Diario.id).all()    
-    texto = db.session.query(Diario.frase).all()
-    id = [e[1] for e in emociones]
-    emociones_lista = [e[0] for e in emociones]
-    entrada = [t[0] for t in texto]  
-        
-    df_plot = pd.DataFrame({
-    'ID': id,
-    'Emocion': emociones_lista,
-    'Frase':entrada
-    })
-
-    import plotly.express as px
-
-    fig = px.scatter(df_plot, x='ID', y='Emocion',title='Distribucion de emociones',hover_data={'ID': False, 'Emocion': False,'Frase':True} )
-
-    graph_html = fig.to_html(full_html=False)
     return render_template('index.html',chart_image=graph_html )
 
+#ruta que crear la entrada y devuelve popup y grafica
 @app.route("/crear-entrada",methods=["POST"])
 def crear():
     
@@ -42,28 +40,22 @@ def crear():
     entrada = Diario(frase=request.form["sentimientos"],emocion=pred_emocion,fecha=fecha_obj)
     db.session.add(entrada)
     db.session.commit()
-    return redirect(url_for('home'))
 
-@app.route('/pop_up',methods=['POST'])
-def pop_up():
-    
-    
-    frase  = request.form(['sentimientos'])
-    emocion = session.get('pred_emocion')
-    
     consejos = {
     'tristeza':'Es normal tener dias tristes, aprender de ellos te ayudsará mucho',
-    'felicidad': 'Disfruta este momento de algria al máximo',
+    'felicidad': 'Disfruta este momento de alegria al máximo',
     'amor': 'Dicen que el amor mueve montañas.¿Será verdad?',
     'enfado': 'La mejor forma de reducir el enfado es respirar hasta 10',
-    'miedo': 'EL ser humano solo tiene dos miedos innatos: A las alturas y a los ruidos fuertes',
+    'miedo': 'El ser humano solo tiene dos miedos innatos: A las alturas y a los ruidos fuertes',
     'sorpresa':'Las sorpresas no te las esperas'
     }
     
-    consejo = consejos.get(emocion)
-    
-    return render_template('index.html',frase=frase,emocion=emocion,consejo=consejo)
+    consejo = consejos.get(pred_emocion)
+    graph_html= generar_grafico()
 
+    return render_template('index.html',frase=request.form["sentimientos"],emocion=pred_emocion,consejo=consejo,chart_image=graph_html)
+
+        
 if __name__=="__main__":
     db.Base.metadata.create_all(bind=db.engine)
     app.run(debug=True)
